9a850e858c32dc2bdd1a30ea5348df36bf28b6da
daomingzhu <596167028@qq.com>
2018-04-15 14:55:37 +0800

    init param

--- ming-parent/web/src/main/java/com/yss/zdm/web/WebApplication.java
+++ ming-parent/web/src/main/java/com/yss/zdm/web/WebApplication.java
@@ -2,10 +2,10 @@ package com.yss.zdm.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class WebApplication {
 
     public static void main(String[] args) {
